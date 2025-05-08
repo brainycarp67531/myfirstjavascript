cefb3e4945735b4a57c7293d0749945db404c57b
Björn Sterling <bjorn.sterling@gmail.com>
2025-05-08 17:11:01 +0200

    started with logical operators

---
+++ logicalOperators.js
@@ -0,0 +1,12 @@
+// https://javascript.info/logical-operators
+
+let a = true;
+let b = false;
+
+let result = a || b;
+console.log(result);
+
+console.log( true || true );   // true
+console.log( false || true );  // true
+console.log( true || false );  // true
+console.log( false || false ); // false
