d15ca432e19920c3f3d0b2bb335784ce62535794
Björn Sterling <bjorn.sterling@gmail.com>
2025-05-10 07:16:43 +0200

    completed the logical operators and created the file for making decisions

---
+++ 04_MakingDecisions.js
@@ -0,0 +1,5 @@
+// https://www.theodinproject.com/lessons/foundations-data-types-and-conditionals#conditionals
+
+// https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Conditionals
+
+
--- logicalOperators.js
+++ logicalOperators.js
@@ -10,3 +10,32 @@ console.log( true || true );   // true
 console.log( false || true );  // true
 console.log( true || false );  // true
 console.log( false || false ); // false
+
+if (1 || 0) {
+    alert( 'truthy');
+}
+
+
+let hour = 12;
+let isWeekend = true;
+
+if (hour < 10 || hour > 18 || isWeekend) {
+    alert( 'The office is closed. '); // it is the weekend
+}
+
+let firstName = "";
+let lastName = "";
+let nickName = "SuperCoder";
+
+alert( firstName || lastName || nickName || "Anonymous"); // SuperCoder
+
+
+// The && (AND) operator
+
+result = a && b;
+
+alert( true && true );
+alert( false && true );
+alert( true && false );
+alert( false && false );
+
