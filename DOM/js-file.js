711049556f74e054616df1b75e2f5e309c8cb999
Björn Sterling <bjorn.sterling@gmail.com>
2025-06-24 18:16:01 +0200

    create DOM with javascript

--- DOM/js-file.js
+++ DOM/js-file.js
@@ -33,5 +33,40 @@ container.appendChild(newParagraph);
 const heading = document.createElement("h3");
 
 // set the text inside the <h3> element
+heading.textContent = "I am a blue h3!";
+
+// set the text in <h3> to blue. 
+heading.style.color = "blue";
+
+// Append the new <h3> as a child of the container
+container.appendChild(heading);
 
 
+// create a new <div> element.
+const newDiv = document.createElement("div");
+
+// Set the border to black and the background to pink of the new <div>
+newDiv.style.border = "1px solid black";
+newDiv.style.backgroundColor = "pink";
+
+// set the class 'secondDiv' to the new div;
+newDiv.classList.add("secondDiv");
+
+// Create the new <h1> element that will go inside the new <div>
+const h1Heading = document.createElement("h1");
+
+// set the text in the <h1> element
+h1Heading.textContent = "I am a div!";
+
+// Create the new <p> element the will go inside the new <div>
+const secondParagraph = document.createElement("p");
+
+// set the text in the <p> element. 
+secondParagraph.textContent = "ME TOO";
+
+// append the new <h1> and <p> element to the new <div>
+newDiv.appendChild(h1Heading);
+newDiv.appendChild(secondParagraph);
+
+// Append the new <div> as a child to the container
+container.appendChild(newDiv);
