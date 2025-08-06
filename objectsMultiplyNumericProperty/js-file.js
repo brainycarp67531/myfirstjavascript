f682adf254fdf033ac14f4b98598044a564fc2c6
brainycarp67531 <108860913+brainycarp67531@users.noreply.github.com>
2025-08-06 12:15:20 +0200

    mulitiply function created

---
+++ objectsMultiplyNumericProperty/js-file.js
@@ -0,0 +1,34 @@
+function multiplyNumeric(object) {
+    // loop over each of the properties in the object
+    for (let key in object) {
+        // check if the object is a number 
+        if (typeof(object[key]) === "number") {
+            // if it is a number the multiply by two 
+            menu[key] = menu[key] * 2;
+        };
+    }
+}
+
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+// Start values
+console.log("Before the call...")
+console.log(menu.title);
+console.log(menu.height);
+console.log(menu.width);
+
+// running the multiplier
+console.log("Multiplier...")
+multiplyNumeric(menu);
+
+// after the multiplication...
+console.log("After the call...")
+console.log(menu.title);
+console.log(menu.height);
+console.log(menu.width);
