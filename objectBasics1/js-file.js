b98a5f43cab67d1dd1f02de3f1a8435076a1dda7
brainycarp67531 <108860913+brainycarp67531@users.noreply.github.com>
2025-08-07 13:13:32 +0200

    Add object basics example with HTML and JS
    Introduced a new 'objectBasics1' directory containing an HTML file and a JavaScript file. The example demonstrates basic object usage in JavaScript, including property access, method invocation, and DOM manipulation to display object data.

---
+++ objectBasics1/js-file.js
@@ -0,0 +1,27 @@
+const cat = {
+  name: "Bertie",
+  breed: "Cymric",
+  color: "white",
+  greeting: function () {
+    console.log("Meow!");
+  },
+};
+
+// Don't edit the code above here!
+
+// Add your code here
+const catName = cat.name;
+
+cat.greeting();
+
+cat.color = "Black";
+
+// Don't edit the code below here!
+
+const section = document.querySelector("section");
+let para1 = document.createElement("p");
+let para2 = document.createElement("p");
+para1.textContent = `The cat's name is ${catName}.`;
+para2.textContent = `The cat's color is ${cat.color}.`;
+section.appendChild(para1);
+section.appendChild(para2);
