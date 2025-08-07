17cf3f1475a7066955ee90d6364c174233ea2b69
brainycarp67531 <108860913+brainycarp67531@users.noreply.github.com>
2025-08-07 14:50:07 +0200

    Add objectBasics4 example with Cat constructor
    Added a new objectBasics4 directory containing an HTML file and a JavaScript file demonstrating object constructors in JavaScript. Also added VS Code settings to ignore the word 'Elfie' in spell checking.

---
+++ objectBasics4/js-file.js
@@ -0,0 +1,18 @@
+function Cat(name, breed, color) {
+    this.name = name;
+    this.breed = breed;
+    this.color = color;
+    this.greeting = function() {
+    console.log(`Hello, said ${this.name} the ${this.breed}.`);
+  }
+};
+
+
+const cat = new Cat("Bertie", "Cymric", "white");
+const cat2 = new Cat("Elfie", "Aphrodite Giant", "ginger");
+
+
+// Don't edit the code below here!
+
+cat.greeting();
+cat2.greeting();
